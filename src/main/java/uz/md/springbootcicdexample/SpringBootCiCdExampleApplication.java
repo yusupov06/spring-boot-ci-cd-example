12b2623c5fb5e2b77fa7e9c5ed6bb34075e6cc16
JaloliddinXabibjonov <jaloliddin0292@gmail.com>
2023-07-21 17:43:47 +0500

    initial commit

--- src/main/java/uz/md/springbootcicdexample/SpringBootCiCdExampleApplication.java
+++ src/main/java/uz/md/springbootcicdexample/SpringBootCiCdExampleApplication.java
@@ -9,5 +9,4 @@ public class SpringBootCiCdExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCiCdExampleApplication.class, args);
     }
-
 }
